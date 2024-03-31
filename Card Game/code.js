8516c5852269f8aa62031d489468ed7f7805bae2
Kelvin Lok <kelvin.l@stalis.com>
2024-03-31 12:00:11 +0100

    Add leader board

--- Card Game/code.js
+++ Card Game/code.js
@@ -1,3 +1,10 @@
+class Score {
+    constructor(player, ticks) {
+        this.player = player;
+        this.ticks = ticks;
+    }
+}
+
 class Card {
     constructor(element) {
         this.element = element;
@@ -58,12 +65,13 @@ class CardGame {
         this.isStarted = false;
         this.totalTicks = 0;
         this.intervalId;
+        this.player = '';
     }
 
     init() {
         let gameBoard = document.getElementById('game-board');
 
-        for (let i = 0; i < 8; i++) {
+        for (let i = 0; i < this.numberOfCards; i++) {
             gameBoard.innerHTML += `<div class="card-wrapper">
                 <div class="card">
                     <div class="card-face no-select card-face-front"></div>
@@ -109,12 +117,16 @@ class CardGame {
             if (this.intervalId) {
                 clearInterval(this.intervalId);
             }
+
+            this.updateLeaderBoard(this.player, this.totalTicks);
         }
 
         this.debugPrint();
     }
 
-    start() {
+    start(player) {
+        this.player = player;
+
         this.unFlipAllCards();
         this.cards.forEach((card) => {
             card.reset();
@@ -131,6 +143,8 @@ class CardGame {
         }
 
         this.intervalId = setInterval(() => { this.countDown(); }, 100);
+
+        this.debugPrint();
     }
 
     countDown() {
@@ -140,11 +154,11 @@ class CardGame {
 
         this.totalTicks = this.totalTicks + 1;
         if (this.totalTicks % 10 == 0) {
-            secondsLabel.innerHTML = this.pad((this.totalTicks / 10) % 60);
-            minutesLabel.innerHTML = this.pad(parseInt((this.totalTicks / 10) / 60));
+            secondsLabel.innerHTML = pad((this.totalTicks / 10) % 60);
+            minutesLabel.innerHTML = pad(parseInt((this.totalTicks / 10) / 60));
         }
 
-        ticksLabel.innerHTML = this.pad(this.totalTicks);
+        ticksLabel.innerHTML = pad(this.totalTicks);
     }
 
     resetCountDown() {
@@ -155,16 +169,6 @@ class CardGame {
         secondsLabel.innerHTML = '00';
         minutesLabel.innerHTML = '00';
         ticksLabel.innerHTML = '00';
-    }
-
-    pad(val) {
-        let valString = val + "";
-
-        if (valString.length < 2) {
-            return "0" + valString;
-        } else {
-            return valString;
-        }
     }
 
     assignCardNumber() {
@@ -243,26 +247,139 @@ class CardGame {
         }
     }
 
+    updateLeaderBoard(player, ticks) {
+        getScores().then(data => {
+            let newScore = new Score(player, ticks);
+            data.scores.push(newScore);
+
+            // sort scores
+            data.scores.sort((first, second) => first.ticks - second.ticks);
+
+            // get the first 20 highest scores
+            const newScores = data.scores.slice(0, 20);
+
+            updateScore(newScores).then(data => {
+            });
+        });
+    }
+
     debugPrint() {
         console.clear();
         console.log('isStarted', this.isStarted);
-        console.log('hasAllCardMatched', this.hasAllCardMatched())
+        console.log('hasAllCardMatched', this.hasAllCardMatched());
+        console.log('player', this.player);
         this.cards.forEach((card) => {
             console.log(card);
         });
     }
 }
 
-const numberOfCards = 8
+const numberOfCards = 8;
 const cardGame = new CardGame(numberOfCards);
+const apiUrl = 'https://api.jsonstorage.net/v1/json/5725bb31-8a05-4754-ba35-8f12024e78e4/f0c0ab30-1a01-42a5-b146-361e7a6045de';
+const apiKey= '8969a471-87b7-4758-9f34-b090d396d9bb';
 
 window.onload = function () {
     cardGame.init();
+
+    const random_uuid = uuidv4();
+    let name = document.getElementById('name-input');
+
+    name.value = random_uuid;
 };
 
 function startGame(evt) {
     evt.preventDefault();
-    
-    cardGame.start();
-    cardGame.debugPrint();
+
+    let name = document.getElementById('name-input');
+    cardGame.start(name.value);
+}
+
+function showLeaderBoard(evt) {
+    evt.preventDefault();
+
+    let leaderBoard = document.getElementById('leader-board');
+
+    if (leaderBoard.style.visibility == 'visible') {
+        leaderBoard.style.visibility = 'hidden';
+    }
+    else {
+        leaderBoard.style.visibility = 'visible';
+    }
+
+    getScores().then(data => {
+        document.getElementById('leader-board-table-wrapper').innerHTML = [
+            '<table id="leader-board-table"><thead>',
+            // ...Object.keys(data.scores[0]).map(key => `<th>${key}</th>`),
+            '<th>Player</th><th>Time (minutes:seconds)</th>',
+            '</thead><tbody>',
+            ...data.scores.map(item => `<tr><td>${item.player}</td><td>${convertToMinutesSeconds(item.ticks)}</td></tr>`),
+            '</tbody></table>'].join("");
+    });
+}
+
+async function getScores() {
+    const response = await fetch(apiUrl);
+
+    if (!response) {
+        throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+
+    return data;
+}
+
+async function updateScore(newScores) {
+    const requestOptions = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ scores: newScores }),
+    };
+
+    fetch(`${apiUrl}?apiKey=${apiKey}`, requestOptions);
+
+    if (!response) {
+        throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+
+    return data;
+}
+
+function convertToMinutesSeconds(ticks) {
+    const totalSeconds = (ticks / 10);
+    let minutes = Math.floor(totalSeconds / 60);
+    const seconds = Math.floor(totalSeconds - (minutes * 60));
+
+    return `${pad(minutes)}:${pad(seconds)}`;
+}
+
+function pad(val) {
+    let valString = val + "";
+
+    if (valString.length < 2) {
+        return "0" + valString;
+    } else {
+        return valString;
+    }
+}
+
+function closeLeaderBoard(evt) {
+    evt.preventDefault();
+
+    let leaderBoard = document.getElementById('leader-board');
+    leaderBoard.style.visibility = 'hidden';
+}
+
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+        .replace(/[xy]/g, function (c) {
+            const r = Math.random() * 16 | 0,
+                v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
 }
